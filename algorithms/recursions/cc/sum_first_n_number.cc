218bf7dcf9f03765207b41a8147f0c42b9247739
Nitin Sahu <sahunitin970@gmail.com>
2025-11-19 14:07:53 +0530

    Refactor recursive sum functions in C++ and TypeScript; update main logic and remove unused code

--- algorithms/recursions/cc/sum_first_n_number.cc
+++ algorithms/recursions/cc/sum_first_n_number.cc
@@ -2,14 +2,23 @@
 using namespace std;
 
 
-void sumOfFirstNNumber(int num, int sum) {
-  if(num < 1) {
-    cout << sum << endl;
-    return;
+// void sumOfFirstNNumber(int num, int sum) {
+//   if(num < 1) {
+//     cout << sum << endl;
+//     return;
+//   }
+//   sumOfFirstNNumber(num - 1, sum + num);
+// }
+
+int sumOfFirstNNumber(int num) {
+  if(num == 0) {
+    return 0;
   }
-  sumOfFirstNNumber(num - 1, sum + num);
+  return num + sumOfFirstNNumber(num - 1);
 }
 int main() {
-  sumOfFirstNNumber(5, 0);
+  // sumOfFirstNNumber(5, 0);
+  int sum = sumOfFirstNNumber(5);
+  cout << sum << endl;
   return 0;
 }
--- algorithms/recursions/ts/sum_first_n_number.ts
+++ algorithms/recursions/ts/sum_first_n_number.ts
@@ -1,9 +1,17 @@
-function sumFirstNNumber(num: number, sum: number):void {
-  if(num < 1) {
-    console.log("Sum is: ", sum);
-    return;
-  }
-  sumFirstNNumber(num -1, sum + num);
-}
+// function sumFirstNNumber(num: number, sum: number):void {
+//   if(num < 1) {
+//     console.log("Sum is: ", sum);
+//     return;
+//   }
+//   sumFirstNNumber(num -1, sum + num);
+// }
 
-sumFirstNNumber(5, 0);
+function sumFirstNNumber(num: number): number {
+  if (num < 1) {
+    return 0;
+  }
+  return num + sumFirstNNumber(num - 1);
+}
+// sumFirstNNumber(5, 0);
+const sum = sumFirstNNumber(15);
+console.log(sum);
