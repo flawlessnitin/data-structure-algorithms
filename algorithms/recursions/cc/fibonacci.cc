2edcf59319710c7831dee40e6c383c31ba464829
Nitin Sahu <sahunitin970@gmail.com>
2025-11-25 13:12:23 +0530

    Update C++ project settings and enhance Fibonacci implementation; add input/output files for testing

--- algorithms/recursions/cc/fibonacci.cc
+++ algorithms/recursions/cc/fibonacci.cc
@@ -4,12 +4,17 @@ using namespace std;
 int fibonacci(int num) {
   if(num <= 1) {
     return num;
-  }
+  } 
   return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
 int main() {
+  int input;
+  cin >> input;
   int value = fibonacci(2);
+  cout << input << endl;
   cout << value << endl;
+  // cout << "Hello world" << endl;
   return 0;
 }
+
