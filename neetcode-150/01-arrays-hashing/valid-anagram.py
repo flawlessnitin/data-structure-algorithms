0659bcbca9a43f8426a92a33bc632b8a535aa6dd
Nitin Sahu <sahunitin970@gmail.com>
2026-05-27 19:07:35 +0530

    Implement isAnagram method in Solution class to check if two strings are anagrams; add test case for validation

--- neetcode-150/01-arrays-hashing/valid-anagram.py
+++ neetcode-150/01-arrays-hashing/valid-anagram.py
@@ -4,4 +4,29 @@
 
 
 class Solution:
-    pass
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        count = {}
+        for i in range(len(s)):
+            if s[i] in count:
+                count[s[i]] += 1
+            else:
+                count[s[i]] = 1
+        print(count)
+        for i in range(len(t)):
+            if t[i] in count:
+                count[t[i]] -= 1
+            else:
+                return False
+        print(count)
+        for key in count:
+            if count[key] != 0:
+                return False
+        return True
+
+# call the isAnagram
+s = "anagram"
+t = "nagaram"
+solution = Solution()
+print(solution.isAnagram(s, t))
